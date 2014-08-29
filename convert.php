c96d8033f96334b2cbb5eb1fa0d88a4d4db78db3
Evgeniy Ivanov <tyz910@gmail.com>
2014-08-29 11:58:02 +0400

    convert.php

---
+++ convert.php
@@ -0,0 +1,26 @@
+<?php
+
+$mapping = [
+    '{{' => '[[',
+    '}}' => ']]',
+    '{%' => '[%',
+    '%}' => '%]',
+    '{#' => '[#',
+    '#}' => '#]',
+];
+
+convert(__DIR__ . '/Preferences', $mapping);
+convert(__DIR__ . '/Snippets', $mapping);
+convert(__DIR__ . '/Syntaxes', $mapping);
+
+function convert($dir, $mapping)
+{
+    foreach (new DirectoryIterator($dir) as $fileInfo) {
+        if ($fileInfo->isFile()) {
+            $content = file_get_contents($fileInfo->getPathname());
+            $content = str_replace(array_keys($mapping), array_values($mapping), $content);
+            $content = str_replace(array_keys($mapping), array_map('preg_quote', array_values($mapping)), $content);
+            file_put_contents($fileInfo->getPathname(), $content);
+        }
+    }
+}
